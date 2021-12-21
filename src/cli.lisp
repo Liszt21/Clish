98faf07ed3e87eee606c395fe16d2db2282bdfc5
liszt21 <1832666492@qq.com>
2021-12-21 21:33:34 +0800

    feat(cli): add default function

--- src/cli.lisp
+++ src/cli.lisp
@@ -55,7 +55,19 @@
    (doc :initform nil)
    (pre :initform (lambda (&optional command args) (format t "Execute command ~a with arguments ~a...~%" command args)))
    (post :initform (lambda (&optional command args result) (format t "Execute command ~a done ~%" command)))
+   (default :initform (lambda (&rest args) (format t "Command ~A with args: [~{ ~A~} ] not registered~%" (car args) (cdr args))))
    (help :initform (lambda (&optional command args) "Help"))))
+
+(defmethod register-command ((x command-line-interface) key value)
+  (if (keywordp key)
+      (setf (slot-value x (intern (string key) 'clish)) value)
+    (push (cons (intern (string key) 'command) value) (slot-value x 'commands))))
+
+(defmethod helper ((x command-line-interface))
+  (format t "Helper for ~A:~%" (slot-value x 'name))
+  (let ((cmds (slot-value x 'commands)))
+    (concatenate 'string (format nil "Commands:~%")
+                 (join "" (mapcar (lambda (item) (format nil "    ~A: ~A~%" (car item) (get-function-arguments (cdr item)))) cmds)))))
 
 (defmethod execute-command ((x command-line-interface) arguments)
   (let* ((cmds (slot-value x 'commands))
@@ -68,21 +80,13 @@
                 cmd
                 (if fn (get-function-arguments fn) "Command not registered"))
         (if fn
-            (let* ((pre (apply (slot-value x 'pre) (list cmd rest)))
-                   (rst (apply fn rest))
-                   (post (apply (slot-value x 'post) (list cmd rest rst))))
-                  rst)
-            (format t "~%Command ~A not registered~%~A~%" cmd (helper x))))))
-
-(defmethod register-command ((x command-line-interface) key value)
-  (if (keywordp key)
-      (setf (slot-value x (intern (string key) 'clish)) value)
-      (push (cons (intern (string key) 'command) value) (slot-value x 'commands))))
-
-(defmethod helper ((x command-line-interface))
-  (let ((cmds (slot-value x 'commands)))
-    (concatenate 'string (format nil "Commands:~%")
-                 (join "" (mapcar (lambda (item) (format nil "    ~A: ~A~%" (car item) (get-function-arguments (cdr item)))) cmds)))))
+          (let* ((pre (apply (slot-value x 'pre) (list cmd rest)))
+                 (rst (apply fn rest))
+                 (post (apply (slot-value x 'post) (list cmd rest rst))))
+            rst)
+          (if (car args)
+            (apply (slot-value x 'default) args)
+            (helper x))))))
 
 (defmacro defcli (name &rest rest)
   `(progn
